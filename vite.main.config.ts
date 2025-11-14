cdc9cdb00ad2a03eafa5cfa9f0318f1311b47257
shy-away <shy-away@tuta.io>
2025-11-14 12:09:23 -0600

    chore: overhaul dependencies, install Tailwind package.json -- add "type": "module", include TS compiler in start script, update versions of many packages tsconfig.json -- use modern react-jsx, use modern ESNext module option, other changes ***Other changes in tsconfig.json were made by agentic AI (Claude Haiku 4.5). I acknowledge I don't understand every change made to this file.*** vite.main.config.ts -- configure to output ES6 (no `require()`) vite.preload.config.ts -- configure external dependency vite.renderer.config.ts -- configure react and tailwind plugins main.ts -- update derivation of `__dirname`, minor reformatting index.css -- use tailwind
    note to self: do all of this in separate commits next time :upside-down:

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,4 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    target: "esnext",
+    lib: {
+      entry: "electron/main.ts",
+      fileName: "main",
+      formats: ["es"],
+    },
+  },
+});
